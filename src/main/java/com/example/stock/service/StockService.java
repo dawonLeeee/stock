77a2457999bee957d22d7ea4615758caed14a6d6
dawon <ekdnjs210@gmail.com>
2024-07-15 21:58:49 +0900

    fixed untracked files

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -14,8 +14,15 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    @Transactional
-    public void decrease(Long id, Long quantity) {
+//    @Transactional
+    public synchronized void decrease(Long id, Long quantity) {
+        /* synchronized 키워드를 사용하여 1개의 스레드만 접근할 수 있도록 설정
+        * BUT 테스트케이스 실해
+        * 왜? @Transactional 때문인데, @Transactional은 기본적으로 메소드에 대해 프록시를 생성하고, 프록시는 메소드 호출을 감싸서 트랜잭션을 시작하고 종료함
+        * 그래서 synchronized 키워드가 메소드에 적용되지 않음
+        * 해결: @Transactional 어노테이션을 주석처리
+        * */
+
         // Stock 조회
         // 재고 감소시킨뒤
         // 갱신된 값 저장
